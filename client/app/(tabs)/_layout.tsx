2942c7bab0263f849b10d66b236ea79138a679b4
ghulamhassan120 <ghulamhassan.dev6@gmail.com>
2026-09-16 12:29:52 -0700

    Cart Screen Ui Complete

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 import { Tabs } from 'expo-router'
 import { Feather, Ionicons } from '@expo/vector-icons'
 import { COLORS } from '@/constants'
+import { useCart } from '@/context/cartContext'
 
 export default function Tabs_layout() {
+    const {cartItems}=useCart()
   return (
     <Tabs
     screenOptions={{
@@ -27,9 +29,17 @@ export default function Tabs_layout() {
             />
         }}/>
         <Tabs.Screen name='cart' options={{
-            tabBarIcon:({color,focused})=> <Feather name={focused?"shopping-cart":"shopping-cart"}
-            size={26} color={color}
-            />
+            tabBarIcon:({color,focused})=> (
+                <View className='relative'>
+                    <Feather name={focused?"shopping-cart":"shopping-cart"}
+            size={26} color={color}/>
+
+                {cartItems.length>0&& (<View className='absolute  -top-2 -right-2 bg-accent size-3 rounded-full items-center justify-center'>
+                        <Ionicons name='ellipse' size={8} color={'white'}/>
+                </View>)}
+
+                </View>
+            )
         }}/>
         <Tabs.Screen name='favouite' options={{
             tabBarIcon:({color,focused})=> <Ionicons name={focused?"heart":"heart-outline"}
